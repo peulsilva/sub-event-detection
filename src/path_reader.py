382a6a4214ab034a7e95caeee2eb353a4be01abf
peulsilva <peulsilva9@gmail.com>
2024-11-15 21:56:42 +0100

    initial commit

---
+++ src/path_reader.py
@@ -0,0 +1,2 @@
+BASE_PATH_TRAIN = "sub-event-detection-in-twitter-streams/challenge_data/train_tweets"
+BASE_PATH_TEST = "sub-event-detection-in-twitter-streams/challenge_data/test_tweets"
---
+++ src/utils.py
@@ -0,0 +1,22 @@
+from src.path_reader import BASE_PATH_TRAIN
+from tqdm import tqdm
+import pandas as pd
+import os
+
+TRAIN_IDX = [0, 2, 4, 7, 8, 11, 13, 14, 18, 19]
+TEST_IDX = [1, 3, 5, 10, 12, 17]
+
+
+def train_test_split():
+    all_dfs = []
+
+    for file in tqdm(os.listdir(BASE_PATH_TRAIN)):
+        file_path = os.path.join(BASE_PATH_TRAIN, file)
+        all_dfs.append(pd.read_csv(file_path))
+
+    all_df = pd.concat(all_dfs)
+
+    train_df = {key: group for key, group in all_df.query(f"MatchID in {TRAIN_IDX}").groupby("MatchID")}
+    test_df = {key: group for key, group in all_df.query(f"MatchID in {TEST_IDX}").groupby("MatchID")}
+
+    return train_df, test_df
